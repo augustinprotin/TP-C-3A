5bbd7a2de2962a90eb9db822f747fab5dc63a1c9
augustin <augugu78@gmail.com>
2024-12-11 11:09:07 +0100

    debug classe chambre 2

--- reservation.cpp
+++ reservation.cpp
@@ -10,7 +10,7 @@ Reservation::Reservation() : _client(0, "", ""),
 Reservation::Reservation(Hotel hotel, Chambre room, Client client, Date date, int nights, int price)
       : _hotel(hotel),_room(room),_client(client),_date(date),_nights(nights),_price(price) {};
 bool Reservation::valid_room() {
-
+      if
 }
 
 bool Reservation::free_room() {
