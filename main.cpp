da77f5316591f7f4ee526fc2da28f07d34376b21
elBaptiste <villeret.baptiste@gmail.com>
2024-12-11 11:54:17 +0100

    modification (details)

--- main.cpp
+++ main.cpp
@@ -4,8 +4,10 @@
 //
 #include <iostream>
 
+#include "reservation.h"
 #include "date.h"
 #include "client.h"
+#include "hotel.h"
 
 int main() {
     std::cout<<("Hello World\n");
@@ -14,5 +16,6 @@ int main() {
     Client augustin(1,"Protin","Augustin");
     std::cout<<augustin.getName()<<"\n";
     std::cout<<augustin.toString()<<"\n";
+
 
 }
--- reservation.cpp
+++ reservation.cpp
@@ -9,11 +9,13 @@
 Reservation::Reservation() : _client(0, "", ""),
       _date(0, 0, 0),
       _hotel("","","",0),
-      _room(0, probleme, 0.0) {}
+      _room(0, probleme, 0.0),
+      _nights(0),
+      _price(0) {}
 Reservation::Reservation(Hotel hotel, Chambre room, Client client, Date date, int nights, int price)
       : _hotel(hotel),_room(room),_client(client),_date(date),_nights(nights),_price(price) {};
 bool Reservation::valid_room() {
-      if (std::find(_hotel.getResa().begin(),_hotel.getResa().end(), _chambre) != _hotel.getResa()) {//fonction pour savoir si la chambre est dans la liste des chambres de l hotel
+      if (std::find(_hotel.getResa().begin(),_hotel.getResa().end(), _room) != _hotel.getResa().end) {//fonction pour savoir si la chambre est dans la liste des chambres de l hotel
             return true;
       }
       return false;
@@ -26,12 +28,10 @@ bool Reservation::free_room() {
 std::string Reservation::to_string() {
       return "Réservation pour M/Mme " + _client.toString() + "\n" +
            "A partir du : " + _date.toString() + "\n" +
-           "Pour une durée de : " + _nights + " nuits\n" +
+           "Pour une durée de : " + std::to_string(_nights) + " nuits\n" +
            "Dans l'Hôtel : " + _hotel.toString() + "\n" +
-           "Et dans la Chambre : " + _chambre.toString();
+           "Et dans la Chambre : " + _room.toString();
 }
-
-get_hotel
 
 
 
