595e6837fb308ba9fbda6993b4360ad13577731f
elBaptiste <villeret.baptiste@gmail.com>
2024-12-11 08:51:50 +0100

    ajout classe date vierge et modification makelist

---
+++ date.cpp
@@ -0,0 +1,5 @@
+//
+// Created by bapti on 11/12/2024.
+//
+
+#include "date.h"
---
+++ date.h
@@ -0,0 +1,16 @@
+//
+// Created by bapti on 11/12/2024.
+//
+
+#ifndef DATE_H
+#define DATE_H
+
+
+
+class date {
+
+};
+
+
+
+#endif //DATE_H
